f04a156689b0e002a309ceaa20c02058e5b5a7dc
Chinmoy Bhuyan <b17cs082@cit.ac.in>
2025-07-25 18:38:03 +0530

    Add reset test

---
+++ tests/test_reset.cpp
@@ -0,0 +1,8 @@
+#include <gtest/gtest.h>
+#define ARDUINO
+#include "arduino_stubs.hpp"
+#include "port/esp32s3/qca7000.hpp"
+
+TEST(Qca7000ResetSimple, ReturnsTrue) {
+    ASSERT_TRUE(qca7000ResetAndCheck());
+}
