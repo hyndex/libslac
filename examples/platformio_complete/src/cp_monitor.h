48c3d61752af136c450f58ec9472374737716ec5
Chinmoy Bhuyan <b17cs082@cit.ac.in>
2025-07-29 01:57:50 +0530

    Improve CP monitoring and PWM logic

--- examples/platformio_complete/src/cp_monitor.h
+++ examples/platformio_complete/src/cp_monitor.h
@@ -5,14 +5,18 @@
 enum CpSubState : uint8_t { CP_A, CP_B1, CP_B2, CP_B3, CP_C, CP_D, CP_E, CP_F };
 
 void     cpMonitorInit();
+#if CP_USE_DMA_ADC
+void     cpLowRateStart(uint32_t period_ms = 5);
+void     cpLowRateStop();
+static inline void cpFastSampleStart() {}
+static inline void cpFastSampleStop() {}
+void     cpDmaStart();
+void     cpDmaStop();
+#else
 void     cpLowRateStart(uint32_t period_ms = 5);
 void     cpLowRateStop();
 void     cpFastSampleStart();
 void     cpFastSampleStop();
-#if CP_USE_DMA_ADC
-void     cpDmaStart();
-void     cpDmaStop();
-#else
 static inline void cpDmaStart() {}
 static inline void cpDmaStop() {}
 #endif
