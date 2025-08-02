ee85393d60e69392ec4b44d9122711fec903dd3f
Chinmoy Bhuyan <b17cs082@cit.ac.in>
2025-08-02 17:16:32 +0530

    Clean platformio example and remove tests

--- tests/freertos/FreeRTOS.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <stdint.h>
-#define pdTRUE 1
-#define pdFALSE 0
-#define pdMS_TO_TICKS(ms) (ms)
-typedef void* TimerHandle_t;
-typedef void (*TimerCallbackFunction_t)(TimerHandle_t);
--- tests/soc/ledc_struct.h
+++
@@ -1,2 +0,0 @@
-#pragma once
-#include "driver/ledc.h"
